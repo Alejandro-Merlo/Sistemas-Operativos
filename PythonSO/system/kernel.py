807c25a29339fd6bd3381194145ee379359a7b96
Alejandro-Merlo <merloalejandro@gmail.com>
2013-10-23 21:12:26 -0300

    Refactorizo el proyecto ya que no estaba hecho con respecto a las especificaciones de los profesores.

--- PythonSO/system/kernel.py
+++ PythonSO/system/kernel.py
@@ -4,76 +4,80 @@ Created on 19/10/2013
 @author: Alejandro
 '''
 from program import Program
-from process import Process
-from shell import Shell
+from instruction_io import InstructionIO
+from instruction_cpu import InstructionCPU
+from algorithm.fifo import FIFO
+from cpu import CPU
+from io_handler import IOHandler
+from pcb import PCB
 from scheduler import Scheduler
-from algorithm.prioritary_round_robin import PrioritaryRoundRobin
-from threading import Semaphore
 from time import sleep
+from threading import Semaphore
 import random
 
 class Kernel():
     
-    def __init__(self):
-        self.ready_list      = []
-        self.ready_io        = []
-        self.scheduler       = Scheduler(PrioritaryRoundRobin(3, 5))
-        self.process_running = None
-        self.main_semaphore  = Semaphore()
+    def __init__(self, algorithm, cpu_semaphore):
+        self.ready_list     = []
+        self.scheduler      = Scheduler(algorithm)
+        self.cpu_semaphore  = cpu_semaphore
+        self.cpu            = CPU(self, cpu_semaphore)
+        self.io_handler     = IOHandler(self)
+        self.global_id      = 0
         
-        self.global_id = 0
-        self.global_pc = 0
+        self.cpu.start()
+        self.io_handler.start()
     
-    def load_program(self, program):
-        process                = Process(program, self.global_id, self.global_pc, random.randint(0, 9))
+    def load(self, program):
+        process                = PCB(program, self.global_id, random.randint(0, 9))
         self.global_id         = self.global_id + 1
-        self.global_pc         = self.global_pc + process.program.instructions.__len__()
         process.state          = "Ready"
-        process.main_semaphore = self.main_semaphore
+        self.ready_list.append(process)
+        self.scheduler.add_element(process)
         
-        self.scheduler.add_element(process, self.ready_io)
-
-    def check_last_running(self):
-        if self.process_running is not None:
-            process = self.process_running
-            if process.state is 'Ready':
-                self.scheduler.add_element(process, self.ready_io)
-            if process.state is 'Ready I/O':
-                self.ready_io.append(process)
-
-    def run_next_process(self):        
-        self.check_last_running()
-        process = self.scheduler.choose_next(self.ready_io)
         
-        while process is None:
-            print 'Esperando mas procesos...'
-            sleep(10)
-            process = self.scheduler.choose_next(self.ready_io)
-            
-        self.process_running = process
+    def io_signal(self, pcb):
+        self.io_handler.add(pcb)
+        self.cpu_semaphore.release()
         
-        if process.isAlive():
-            process.semaphore.release()
-        else:
-            process.start()
-            
-    def aging_processes(self):
-        self.scheduler.aging(self.ready_io)
+    def kill_signal(self, pcb):
+        pcb.state = 'Terminated'
+        self.cpu_semaphore.release()
     
-    def update_processes(self):
-        self.scheduler.update(self.ready_io)
+    def ready_signal(self, pcb):
+        self.ready_list.append(pcb)
+        self.scheduler.add_element(pcb)
+
+    def run_next_process(self):
+        process = self.scheduler.choose_next()
+        while process is None:
+            print 'Kernel esperando mas procesos...'
+            sleep(5)
+            process = self.scheduler.choose_next()
+        
+        self.ready_list.remove(process)
+        self.cpu.set_pcb(process)
+                    
+#     def aging_processes(self):
+#         self.scheduler.aging(self.ready_list)
+#     
+#     def update_processes(self):
+#         self.scheduler.update(self.ready_list)
         
 def main():
-    kernel = Kernel()
-    shell = Shell(kernel)
-    shell.start()
+    kernel = Kernel(FIFO(), Semaphore())
+    program1 = Program('Wine', [InstructionCPU('CPU1'), InstructionIO('IO1'), InstructionCPU('CPU2')])
+    program2 = Program('Eclipse', [InstructionIO('IO1'), InstructionCPU('CPU1'), InstructionCPU('CPU2')])
+    program3 = Program('Thor', [InstructionCPU('CPU1'), InstructionCPU('CPU2'), InstructionIO('IO1')])
+    kernel.load(program1)
+    kernel.load(program2)
+    kernel.load(program3)
     
     while True:
-        kernel.update_processes()
-        kernel.main_semaphore.acquire()            
+        kernel.cpu_semaphore.acquire()
+#         kernel.update_processes()
         kernel.run_next_process()
-        kernel.aging_processes()
-        sleep(5)
+#         kernel.aging_processes()
 
 if __name__ == '__main__':
     main()
